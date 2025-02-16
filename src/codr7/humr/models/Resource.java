6365143b33f3adcef7401740193b353e7b5456a7
Andreas Nilsson <codr7@protonmail.com>
2025-02-16 01:19:44 +0100

    Store time zone.

--- src/codr7/humr/models/Resource.java
+++ src/codr7/humr/models/Resource.java
@@ -5,7 +5,7 @@ import codr7.humr.Context;
 import codr7.tyred.Record;
 import codr7.tyred.Table;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public class Resource extends Model {
     public Resource(final Context cx) {
@@ -13,7 +13,7 @@ public class Resource extends Model {
 
         record()
                 .set(db.resourceId, db.resourceIds.nextValue(cx.dbContext))
-                .set(db.resourceCreatedAt, LocalDateTime.now())
+                .set(db.resourceCreatedAt, OffsetDateTime.now())
                 .set(db.resourceCreatedBy, cx.currentUser().record());
     }
 
